02ebd7828b350f028a1e9c7160a03d24ea4e22dc
96hsjeong <96.hs.jeong@gmail.com>
2023-03-15 14:00:34 +0900

    Update: 백엔드 프로젝트 생성

---
+++ backend/psg/src/main/java/com/ai4ai/psg/PsgApplication.java
@@ -0,0 +1,13 @@
+package com.ai4ai.psg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsgApplication.class, args);
+	}
+
+}
