fef381ae95ef3c9718b11b6f20e92c487a3e93f9
김준형 <gittgi768@gmail.com>
2023-03-21 10:56:34 +0900

    [S08P22A803-153] Feat: AppBar 구현

---
+++ frontend/lib/screens/search/text_search_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextSearchPage extends StatelessWidget {
+  const TextSearchPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text('durl'));
+  }
+}
