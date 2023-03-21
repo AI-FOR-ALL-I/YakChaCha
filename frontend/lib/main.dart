08de1a9af6cffa4c03c404330c1af51e39de638a
2yoonjinny <2yoonjinny@naver.com>
2023-03-21 14:54:11 +0900

    [S08P22A803-135] Feat: 카카오로그인 테스트

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/bottom_navigation.dart';
-import 'package:frontend/widgets/text_search/text_search_pill_to_register.dart';
 
 void main() {
+  //KakaoSdk.init(nativeAppKey: "c940f1badb47a0c2cb210d71a84009fb");
   runApp(const MyApp());
 }
 
@@ -16,16 +16,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: const ColorScheme(
             brightness: Brightness.light,
-            primary: Color(0xFFBBE4CB),      // Main
-            onPrimary: Color(0xFFFBFFFB),    // little-white
-            secondary: Color(0xFF666666),    // Gray
-            onSecondary: Color(0xFFE1E1E1),  // Light-Gray
-            error: Color(0xFFFF6961),        // Error-Red
-            onError: Color(0xFFF7CA66),      // Warning-yellow
-            background: Color(0xFF4AC990),   // Success-green
+            primary: Color(0xFFBBE4CB), // Main
+            onPrimary: Color(0xFFFBFFFB), // little-white
+            secondary: Color(0xFF666666), // Gray
+            onSecondary: Color(0xFFE1E1E1), // Light-Gray
+            error: Color(0xFFFF6961), // Error-Red
+            onError: Color(0xFFF7CA66), // Warning-yellow
+            background: Color(0xFF4AC990), // Success-green
             onBackground: Color(0xFF6694F7), // Info - blue
-            surface: Color(0xFFBABABA),      // Placeholder
-            onSurface: Color(0xFF848293)),   // Point-Purple
+            surface: Color(0xFFBABABA), // Placeholder
+            onSurface: Color(0xFF848293)), // Point-Purple
       ),
       title: 'bottomNavigationBar Test',
       home: const BottomNavigation(),
