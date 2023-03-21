e60c50cb7337791a9602437784652564fda20731
cjfal2 <cjfal2@kakao.com>
2023-03-21 10:59:14 +0900

    [S08P22A803-155] Update: 나의 약 to 상세 네비게이션 뚫음 + 약상세 스켈레톤 구현

--- frontend/lib/screens/pill_details/pill_details.dart
+++ frontend/lib/screens/pill_details/pill_details.dart
@@ -8,7 +8,57 @@ class PillDetails extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(title: Text("임시 앱바"),),
+      appBar: AppBar(title: const Text("약 상세"),),
+      body: ListView(
+        children: [
+          Image.asset('assets/images/pills.png',),
+          Column(children: [Text("텍스트 큰거"), Text("텍스트 작은거")]),
+          Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("텍스트"),
+                Icon(Icons.arrow_drop_down_outlined),
+              ],
+            ),
+          ],)
+        ],
+      ),
     );
   }
 }
