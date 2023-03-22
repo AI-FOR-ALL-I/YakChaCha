3a7b8f6cd03c8ca4623b6ae66ec7f58b27958f9b
2yoonjinny <2yoonjinny@naver.com>
2023-03-22 17:23:21 +0900

    [S08P22A803-141] Feat: 내 약 목록 컴포넌트 만들기

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -3,6 +3,10 @@ import 'package:frontend/widgets/main/eat_check_button.dart';
 import 'package:frontend/widgets/main/time_header.dart';
 
 class HomePage extends StatelessWidget {
+  // final List<Map<String, String>> myList = [
+  //   {'title': 'title1'},
+  //   {'title': 'title2'},
+  // ];
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -11,12 +15,12 @@ class HomePage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(0.0),
         child: ListView(
-          children: const [
-            TimeHeader(
+          children: [
+            const TimeHeader(
               nickname: 'user',
               timeline: 0,
             ),
-            Row(
+            const Row(
               children: [
                 Padding(
                   padding: EdgeInsets.all(10.0),
@@ -43,10 +47,44 @@ class HomePage extends StatelessWidget {
               ],
             ),
             // 시간 다가오는지 여부에 따라 활성화?
-            Padding(
+            const Padding(
               padding: EdgeInsets.all(10.0),
               child: EatCheckButton(),
-            )
+            ),
+            const Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Text(
+                "내 약 목록",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16.0),
+              ),
+            ),
+            SizedBox(
+              height: 100,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int index) {
+                    return SizedBox(
+                      width: 200.0,
+                      height: 200.0,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          color: Color(0xFFFFF8EA),
+                        ),
+                        child: Column(
+                          children: [
+                            Image.asset('assets/images/night.png',
+                                width: 100, height: 70, fit: BoxFit.fitWidth),
+                            const Text('타이레놀'),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
           ],
         ),
       ),
