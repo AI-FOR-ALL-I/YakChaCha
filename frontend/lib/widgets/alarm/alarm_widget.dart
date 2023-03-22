bcc365a57667b2e178020d51f243672802f25908
김준형 <gittgi768@gmail.com>
2023-03-22 16:19:45 +0900

    [S08P22A803-162] Feat: 알람 상세페이지(MyPill연동은 미완), TagWidget, 앱바(CutomMainAppBar & SimpleAppBar) 구현

--- frontend/lib/widgets/alarm/alarm_widget.dart
+++ frontend/lib/widgets/alarm/alarm_widget.dart
@@ -1,60 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/alarm/alarm_detail_page.dart';
 
 class CustomAlarmWidget extends StatelessWidget {
   const CustomAlarmWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 319 / 144,
-      child: Container(
-        margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(15)),
-        child: Center(
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => AlarmDetailPage()));
+      },
+      child: AspectRatio(
+        aspectRatio: 319 / 144,
+        child: Container(
+          margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black),
+              borderRadius: BorderRadius.circular(15)),
+          child: Center(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Container(
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.blue)),
-                    child: Text('김준형 복약'),
-                  ),
-                  Container(
-                    child: Text(
-                      '09:30 AM',
-                      style: TextStyle(fontSize: 40),
-                    ),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.yellow)),
-                  ),
-                  Row(
-                    children: [
-                      Container(
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
                           decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Colors.pink,
-                              ),
-                              borderRadius: BorderRadius.circular(15)),
-                          child: Text('태그명 1')),
-                      Text('태그명 2')
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              decoration:
-                  BoxDecoration(border: Border.all(color: Colors.green)),
-              child: Icon(Icons.radio_button_unchecked_outlined),
-              // child: Icon(Icons.check_circle_outlined),
-            ) // 여기가 체크 아이콘
-          ]),
+                              border: Border.all(color: Colors.blue)),
+                          child: Text('김준형 복약'),
+                        ),
+                        Container(
+                          child: Text(
+                            '09:30 AM',
+                            style: TextStyle(fontSize: 40),
+                          ),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.yellow)),
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: Colors.pink,
+                                    ),
+                                    borderRadius: BorderRadius.circular(15)),
+                                child: Text('태그명 1')),
+                            Text('태그명 2')
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.green)),
+                    child: Icon(Icons.radio_button_unchecked_outlined),
+                    // child: Icon(Icons.check_circle_outlined),
+                  ) // 여기가 체크 아이콘
+                ]),
+          ),
         ),
       ),
     );
---
+++ frontend/lib/widgets/common/custom_main_app_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/screens/search/text_search_page.dart';
+
+class CustomMainAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const CustomMainAppBar({Key? key, required this.isMain}) : super(key: key);
+  final bool isMain; // Main화면에서는 투명한 앱바에 하얀 글씨
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+        leading: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Text('YakChaCha',
+                style: (TextStyle(
+                  color: isMain
+                      ? Colors.white
+                      : Theme.of(context).colorScheme.background,
+                  overflow: TextOverflow.visible,
+                )),
+                softWrap: false,
+                maxLines: 1),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search_outlined,
+              color: isMain ? Colors.white : Colors.grey,
+            ),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => TextSearchPage()));
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.notifications_outlined,
+                color: isMain ? Colors.white : Colors.grey),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.person_outlined,
+                color: isMain ? Colors.white : Colors.grey),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: isMain ? Colors.transparent : Colors.white,
+        elevation: 0.0,
+        iconTheme:
+            IconThemeData(color: Theme.of(context).colorScheme.secondary));
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
