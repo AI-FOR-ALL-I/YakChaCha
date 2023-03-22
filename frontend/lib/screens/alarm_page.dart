abbb179da08ca513b2870d63d9629d6f40b54854
김준형 <gittgi768@gmail.com>
2023-03-22 10:53:43 +0900

    [S08P22A803-161] Feat: 알람표시 위젯, 알람리스트페이지 레이아웃만 구현

--- frontend/lib/screens/alarm_page.dart
+++ frontend/lib/screens/alarm_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/alarm/alarm_widget.dart';
 
 class AlarmPage extends StatelessWidget {
   const AlarmPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('This is a alarm setting Page'),
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('알람',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize:
+                      MediaQuery.of(context).size.height >= 720 ? 30 : 16)),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, i) {
+                    return CustomAlarmWidget();
+                  }),
+              Positioned(
+                bottom: 40,
+                right: 40,
+                child: FloatingActionButton(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    onPressed: () {},
+                    child: Text('+', style: TextStyle(color: Colors.white))),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
