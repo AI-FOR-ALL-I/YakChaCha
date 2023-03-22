a3b929b4f85c93bd0dcb0b1adc88f107f096ab0f
2yoonjinny <2yoonjinny@naver.com>
2023-03-22 16:53:15 +0900

    [S08P22A803-141] Feat: 컴포넌트 연결

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -9,9 +9,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(0.0),
         child: ListView(
-          children: const [TimeHeader(timeline: 0), EatCheckButton()],
+          children: const [
+            TimeHeader(
+              nickname: 'user',
+              timeline: 0,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "약 먹어야하는 시간",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0),
+                  ),
+                ),
+                Icon(Icons.schedule, size: 20.0),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                  child: Text(
+                    "17:00",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0),
+                  ),
+                ),
+              ],
+            ),
+            // 시간 다가오는지 여부에 따라 활성화?
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: EatCheckButton(),
+            )
+          ],
         ),
       ),
     );
