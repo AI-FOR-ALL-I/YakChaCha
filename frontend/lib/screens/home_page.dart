c2c4bf98fd96df339fc9248198cafe1c65869fa2
2yoonjinny <2yoonjinny@naver.com>
2023-03-23 14:23:18 +0900

    [S08P22A803-183] Fix: 홈 화면 AppBar 안덮이는문제 해결

--- frontend/lib/screens/home_page.dart
+++ frontend/lib/screens/home_page.dart
@@ -12,81 +12,197 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(0.0),
-        child: ListView(
-          children: [
-            const TimeHeader(
-              nickname: 'user',
-              timeline: 0,
-            ),
-            const Row(
+      body: Column(
+        children: [
+          const TimeHeader(
+            nickname: 'user',
+            timeline: 0,
+          ),
+          Expanded(
+              child: MediaQuery.removePadding(
+            context: context,
+            removeTop: true,
+            child: ListView(
               children: [
-                Padding(
+                const Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(0.0),
+                      child: Text(
+                        "약 먹어야하는 시간",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                    Icon(Icons.schedule, size: 20.0),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                      child: Text(
+                        "17:00",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 16.0),
+                      ),
+                    ),
+                  ],
+                ),
+                // 시간 다가오는지 여부에 따라 활성화?
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: EatCheckButton(),
+                ),
+                const Padding(
                   padding: EdgeInsets.all(10.0),
                   child: Text(
-                    "약 먹어야하는 시간",
+                    "내 약 목록",
                     style: TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.w500,
                         fontSize: 16.0),
                   ),
                 ),
-                Icon(Icons.schedule, size: 20.0),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: 200.0,
+                          height: 200.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFF8EA),
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset('assets/images/night.png',
+                                    width: 100,
+                                    height: 70,
+                                    fit: BoxFit.fitWidth),
+                                const Text('타이레놀'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
                   child: Text(
-                    "17:00",
+                    "내 약 목록",
                     style: TextStyle(
                         color: Colors.black,
                         fontWeight: FontWeight.w500,
                         fontSize: 16.0),
                   ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: 200.0,
+                          height: 200.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFF8EA),
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset('assets/images/night.png',
+                                    width: 100,
+                                    height: 70,
+                                    fit: BoxFit.fitWidth),
+                                const Text('타이레놀'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "내 약 목록",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: 200.0,
+                          height: 200.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFF8EA),
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset('assets/images/night.png',
+                                    width: 100,
+                                    height: 70,
+                                    fit: BoxFit.fitWidth),
+                                const Text('타이레놀'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(
+                    "내 약 목록",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 16.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 3,
+                      itemBuilder: (BuildContext context, int index) {
+                        return SizedBox(
+                          width: 200.0,
+                          height: 200.0,
+                          child: Container(
+                            decoration: const BoxDecoration(
+                              color: Color(0xFFFFF8EA),
+                            ),
+                            child: Column(
+                              children: [
+                                Image.asset('assets/images/night.png',
+                                    width: 100,
+                                    height: 70,
+                                    fit: BoxFit.fitWidth),
+                                const Text('타이레놀'),
+                              ],
+                            ),
+                          ),
+                        );
+                      }),
                 ),
               ],
             ),
-            // 시간 다가오는지 여부에 따라 활성화?
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: EatCheckButton(),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(10.0),
-              child: Text(
-                "내 약 목록",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w500,
-                    fontSize: 16.0),
-              ),
-            ),
-            SizedBox(
-              height: 100,
-              child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 3,
-                  itemBuilder: (BuildContext context, int index) {
-                    return SizedBox(
-                      width: 200.0,
-                      height: 200.0,
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          color: Color(0xFFFFF8EA),
-                        ),
-                        child: Column(
-                          children: [
-                            Image.asset('assets/images/night.png',
-                                width: 100, height: 70, fit: BoxFit.fitWidth),
-                            const Text('타이레놀'),
-                          ],
-                        ),
-                      ),
-                    );
-                  }),
-            ),
-          ],
-        ),
+          ))
+        ],
       ),
     );
   }
