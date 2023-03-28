79cf044197d7c3b8424d528d35eb50fe08f77d3e
cjfal2 <cjfal2@kakao.com>
2023-03-28 16:07:11 +0900

    [S08P22A803-216] Update: 복용중인 약 API 연결 + renewpill 조건부 걸어둠

--- frontend/lib/screens/pill_details/pill_details_for_api.dart
+++ frontend/lib/screens/pill_details/pill_details_for_api.dart
@@ -13,7 +13,7 @@ class PillDetailsForApi extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Future<PillDetailModel> pillDetail = PillDetailApi.getPillDetail(num);
-    
+
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: SimpleAppBar(title: "약 상세"),
@@ -34,6 +34,29 @@ class PillDetailsForApi extends StatelessWidget {
     var pillDetail = snapshot.data!;
     return ListView(
       children: [
+        // 약 등록을 조건부로 걸어줘야함
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 2, horizontal: 30),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "약 등록",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.add_box_outlined,
+                  color: Color(0xFF4AC990),
+                  size: 30,
+                ),
+              )
+            ],
+          ),
+        ),
+        // 약 사진
         Container(
           margin: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
           width: double.infinity,
@@ -43,43 +66,73 @@ class PillDetailsForApi extends StatelessWidget {
           ),
           child: AspectRatio(
               aspectRatio: 2 / 1,
-              child:
-                  Image.network(pillDetail.img, fit: BoxFit.fill)),
+              child: Image.network(pillDetail.img, fit: BoxFit.fill)),
         ),
+
+        // 약 이름 + 제조 회사
         Column(
           children: [
-            Column(
-              children: [
-                Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(vertical: 2, horizontal: 60),
-                  child: Text(
-                    pillDetail.itemName,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w800,
-                    ),
-                  ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.symmetric(vertical: 2, horizontal: 60),
+              child: Text(
+                pillDetail.itemName,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800,
                 ),
-                Container(
-                    margin: EdgeInsets.symmetric(vertical: 2, horizontal: 60),
-                    child: Text(
-                      pillDetail.entpName,
-                      textAlign: TextAlign.center,
-                    )),
-              ],
+              ),
             ),
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 2, horizontal: 60),
+                child: Text(
+                  pillDetail.entpName,
+                  textAlign: TextAlign.center,
+                )),
           ],
         ),
+        // 태그명 & 복용 기간 부분, 조건부로 안보이게 해야함 + 수정가능하도록
+        Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("태그명1",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text("태그명2",
+                  style: TextStyle(fontSize: 20),
+                ),
+                IconButton(onPressed: (){}, icon: Icon(Icons.mode_edit_outlined))
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("복용기간",
+                  style: TextStyle(fontSize: 20),
+                ),
+                Text("2023.03.01 ~ 2023.03.02",
+                  style: TextStyle(fontSize: 20),
+                ),
+                IconButton(onPressed: (){}, icon: Icon(Icons.mode_edit_outlined))
+              ],
+            )
+          ],
+        ),
+        // 상세 설명 부분
         Column(
           children: [
             PillDetailLine(lineTitle: "성분", content: pillDetail.mainItemIngr),
             PillDetailLine(lineTitle: "효능", content: pillDetail.eeDocData),
             PillDetailLine(lineTitle: "복용 방법", content: pillDetail.udDocData),
-            PillDetailLine(lineTitle: "보관 방법", content: pillDetail.storageMethod),
-            PillDetailLine(lineTitle: "복용 시 주의사항", content: pillDetail.nbDocData),
-            PillDetailLine(lineTitle: "함께 먹지 말아야 하는 성분", content: pillDetail.typeCode),
+            PillDetailLine(
+                lineTitle: "보관 방법", content: pillDetail.storageMethod),
+            PillDetailLine(
+                lineTitle: "복용 시 주의사항", content: pillDetail.nbDocData),
+            PillDetailLine(
+                lineTitle: "함께 먹지 말아야 하는 성분", content: pillDetail.typeCode),
           ],
         )
       ],
--- frontend/lib/services/taken_pill_api.dart
+++ frontend/lib/services/taken_pill_api.dart
@@ -4,21 +4,21 @@ import 'package:frontend/models/my_pill_model.dart';
 import 'package:http/http.dart' as http;
 
 
-class MyPillApi {
+class TakenPillApi {
   static const String baseUrl = "http://10.0.2.2:3001";
-  static const String taking = "medicineTaking";
+  static const String taking = "medicineTaken";
 
-  static Future<List<MyPillModel>> getMyPill() async {
-    List<MyPillModel> myPills = [];
+  static Future<List<MyPillModel>> getTakenPill() async {
+    List<MyPillModel> takenPills = [];
     final url = Uri.parse("$baseUrl/$taking");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final List<dynamic> pills = jsonDecode(response.body);
       for (var pill in pills) {
         final instance = MyPillModel.fromJson(pill);
-        myPills.add(instance);
+        takenPills.add(instance);
       }
-      return myPills;
+      return takenPills;
     }
     throw Error();
   }
