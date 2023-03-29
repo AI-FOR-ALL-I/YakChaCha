b15ea7948912161cd8311add70a87cd2597236fe
96hsjeong <96.hs.jeong@gmail.com>
2023-03-29 15:43:03 +0900

    [S08P22A803-239] Update: Redis 설정

--- backend/ycc/src/main/java/com/ai4ai/ycc/util/RedisUtil.java
+++ backend/ycc/src/main/java/com/ai4ai/ycc/util/RedisUtil.java
@@ -7,6 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.TimeUnit;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -14,43 +16,62 @@ public class RedisUtil {
 
     private final RedisTemplate<String, String> redisTemplate;
 
-    public void save(String key, Object data)  {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            String value = objectMapper.writeValueAsString(data);
-            log.info("[Redis] key: {}, value: {}", key, value);
-            redisTemplate.opsForValue().set(key, value);
-            log.info("[Redis] Save Success...");
-        } catch (JsonProcessingException e) {
-            log.warn("[Redis] Save Fail...");
-            throw new RuntimeException(e);
-        }
-    }
-
     public <T> T get(String key, Class<T> valueType) {
         String value = redisTemplate.opsForValue().get(key);
-        log.info("[Redis] key: {}, value: {}", key, value);
+        log.info("[Get] key: {}, value: {}", key, value);
 
         if (value == null) {
-            log.info("[Redis] Not Found Key...");
+            log.info("[Get] Not Found Key...");
             return null;
         }
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             T result = objectMapper.readValue(value, valueType);
-            log.info("[Redis] Get Success...");
+            log.info("[Get] Success...");
             return result;
         } catch(Exception e){
-            log.warn("[Redis] Get Fail...");
+            log.warn("[Get] Fail...");
             return null;
         }
     }
 
+    public void set(String key, Object data)  {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String value = objectMapper.writeValueAsString(data);
+            log.info("[Set] key: {}, value: {}", key, value);
+            redisTemplate.opsForValue().set(key, value);
+            log.info("[Set] Success...");
+        } catch (JsonProcessingException e) {
+            log.warn("[Set] Fail...");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setex(String key, Object data, int expireSeconds)  {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String value = objectMapper.writeValueAsString(data);
+            log.info("[SetEx] key: {}, value: {}, expire: {}s", key, value, expireSeconds);
+            redisTemplate.opsForValue().set(key, value, expireSeconds, TimeUnit.SECONDS);
+            log.info("[SetEx] Success...");
+        } catch (JsonProcessingException e) {
+            log.warn("[SetEx] Fail...");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void expire(String key, int expireSeconds) {
+        log.info("[Expire] Key: {}...", key);
+        redisTemplate.expire(key, expireSeconds, TimeUnit.SECONDS);
+        log.info("[Expire] Success...");
+    }
+
     public void delete(String key) {
-        log.info("[Redis] Delete Key: {}...", key);
+        log.info("[Delete] Key: {}...", key);
         redisTemplate.delete(key);
-        log.info("[Redis] Delete Success...");
+        log.info("[Delete] Success...");
     }
 
 }
