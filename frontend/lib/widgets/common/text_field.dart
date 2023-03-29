62ca3f424d5a2cf555441deb9e4b1ab82b372836
2yoonjinny <2yoonjinny@naver.com>
2023-03-29 12:35:42 +0900

    [S08P22A803-250] Feat: textfield 컴포넌트 터치 영역 처리

--- frontend/lib/widgets/common/text_field.dart
+++ frontend/lib/widgets/common/text_field.dart
@@ -9,12 +9,18 @@ class TextFieldComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        hintText: hintText,
-        border: const OutlineInputBorder(),
+    return GestureDetector(
+      onTap: () {
+        // TextField 영역이 아닌 다른 영역을 탭했을 때 키보드를 닫는 코드 작성
+        FocusScope.of(context).unfocus(); // 키보드 닫기
+      },
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: const OutlineInputBorder(),
+        ),
+        onChanged: onChanged,
       ),
-      onChanged: onChanged,
     );
   }
 }
