952642be693feb13c9401a7e2747aec503e143d6
박장훈 <janghoonp12@naver.com>
2023-03-31 17:34:40 +0900

    [S08P22A803-232] Feat: easyocr api 구현

--- ai/ycc/proj_pill/proj_pill/ocr_prescription.py
+++ ai/ycc/proj_pill/proj_pill/ocr_prescription.py
@@ -1,2 +1,22 @@
-def run_ocr_model(pil_image):
-    return True
+import easyocr
+import re
+
+def run_ocr_model(image):
+    reader = easyocr.Reader(['ko'])
+    result = reader.readtext(image)
+
+    edi_code = []
+    for data in result:
+        string = data[1]
+        numbers = re.findall(r'\d+', string)
+        if numbers:
+            for j in numbers:
+                if len(j) != 9:
+                    continue
+                edi_code.append(j)
+    result = {
+        'edi_code': edi_code
+    }
+
+    print('job done')
+    return result
